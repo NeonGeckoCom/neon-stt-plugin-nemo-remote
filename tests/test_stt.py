14871b371d31b05b222625f1ed4c39ef206f6393
Daniel McKnight <daniel@neon.ai>
2023-11-21 12:10:18 -0800

    InitialImplementation

---
+++ tests/test_stt.py
@@ -0,0 +1,57 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import unittest
+import speech_recognition as sr
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from neon_stt_plugin_nemo_remote import NemoRemoteSTT
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_PATH = os.path.join(ROOT_DIR, "test_audio")
+
+
+class TestGetSTT(unittest.TestCase):
+    def setUp(self) -> None:
+        self.stt = NemoRemoteSTT({'lang': 'en'})
+
+    def test_get_stt(self):
+        r = sr.Recognizer()
+        for file in os.listdir(TEST_PATH):
+            transcription = os.path.splitext(os.path.basename(file))[0].lower()
+            audio_path = os.path.join(TEST_PATH, file)
+            with sr.AudioFile(audio_path) as source:
+                audio = r.record(source)  # read the entire audio file
+                result = self.stt.execute(audio)
+                self.assertIn(transcription, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
